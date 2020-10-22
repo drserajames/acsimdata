3504ffdc036a3d84b1c5f4c55314da25cc8dab81
drserajames <semaj.haras@gmail.com>
2020-10-22 17:07:53 +0100

    Add new arguments to function
    antigen_density as an alternative to specifying range
    rdistribution to allow different random distributions for the positions
    coincident to have antigen and sera in the same positions
    
    changed range to be a single value
    corrected names for serum positions AG->SR

--- R/map_maker.R
+++ R/map_maker.R
@@ -1,24 +1,36 @@
 #' Make a random map
 #'
-#' Create random map positions and return the positions and distance matrix. Currently produces a map as a line, square, cube or hypercube
+#' Create random map positions and return the positions and distance matrix. Currently produces a map as a line, square, cube or hypercube.
 #'
 #' @param n_antigens The number of antigen points
 #' @param n_sera The number of serum points
-#' @param range The range of the map (in all dimensions)
+#' @param range The maximum range of the map (in all dimensions)
 #' @param dimensions The number of dimensions the map
+#' @param antigen_density The density of antigen points, which can be used to indirectly specify the range. Only used when range is not specified
+#' @param rdistribution A function that generates random numbers for the spatial distribution of points. The default is runif and a reasonable option is rnorm or a user generated function.
+#' @param coincident TRUE if the serum points are in the same position as the antigen points, FALSE otherwise
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-#' map_maker_random(5,5)
-map_maker_random <- function(n_antigens = 10, n_sera = 10, range = c(0,10), dimensions = 2){
-  ag_coord <- matrix(runif(n_antigens*dimensions, range[1], range[2]), ncol=dimensions)
+#' map_maker_random(5, 5, 10)
+map_maker_random <- function(n_antigens, n_sera, range = NA, dimensions = 2, antigen_density = n_antigens/range^dimensions,
+                             rdistribution = runif, coincident=T){
+  if(is.na(range)){
+    range <- (n_antigens/density)^(1/dimensions)
+  }
+
+  ag_coord <- matrix(rdistribution(n_antigens*dimensions, 0, range), ncol=dimensions)
   rownames(ag_coord) <- paste0("AG", 1:n_antigens)
 
-  sr_coord <- matrix(runif(n_sera*dimensions, range[1], range[2]), ncol=dimensions)
-  rownames(sr_coord) <- paste0("SR", 1:n_antigens)
+  if (coincident == T){
+    sr_coord <- ag_coord[1:n_sera,]
+  }else{
+  sr_coord <- matrix(rdistribution(n_sera*dimensions, 0, range), ncol=dimensions)
+  }
+  rownames(sr_coord) <- paste0("SR", 1:n_sera)
 
   all_coord <- rbind(ag_coord, sr_coord)
 
