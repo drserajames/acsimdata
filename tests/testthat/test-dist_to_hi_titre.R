162c392737534778b32bd3777465b05aaa984078
drserajames <semaj.haras@gmail.com>
2020-10-23 13:23:37 +0100

    Fix titre calculation
    Fixed an error in the way distances were converted to titres (previouly was just logging them instead of substracting from the maximum titre)
    
    Tidied tests

--- tests/testthat/test-dist_to_hi_titre.R
+++ tests/testthat/test-dist_to_hi_titre.R
@@ -1,85 +1,49 @@
 test_that("output right size", {
-  d <- matrix(100*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d)
-  expect_equal(dim(d), dim(hi$raw_titre))
-  expect_equal(dim(d), dim(hi$hi_titre))
 
-  d <- matrix(10*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d)
-  expect_equal(dim(d), dim(hi$raw_titre))
-  expect_equal(dim(d), dim(hi$hi_titre))
+  m <- NULL
+  d <- NULL
+  size <- c(1,10,100)
+  for (i in 1:3){
+    d[[i]] <- matrix(size[i]*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
+    m[[i]] <- map_maker_random(5,5,size[i])
 
-  d <- matrix(runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d)
-  expect_equal(dim(d), dim(hi$raw_titre))
-  expect_equal(dim(d), dim(hi$hi_titre))
+    expect_equal(dim(d[[i]]), dim(hi$raw_titre))
 
-  m <- map_maker_random(5,5,100)
-  hi <- dist_to_hi_titre(m)
-  expect_equal(dim(m$dist), dim(hi$raw_titre))
-  expect_equal(dim(m$dist)/2, dim(hi$hi_titre))
+    expect_equal(dim(m[[i]]$dist)/2, dim(hi$raw_titre))
+  }
 
-  m <- map_maker_random(5,5,10)
-  hi <- dist_to_hi_titre(m)
-  expect_equal(dim(m$dist), dim(hi$raw_titre))
-  expect_equal(dim(m$dist)/2, dim(hi$hi_titre))
 
-  m <- map_maker_random(5,5,1)
-  hi <- dist_to_hi_titre(m)
-  expect_equal(dim(m$dist), dim(hi$raw_titre))
-  expect_equal(dim(m$dist)/2, dim(hi$hi_titre))
 })
 
-test_that("titre divisible by divisor and base", {
+test_that("titre divisible by divisor and base wih default min_log_titre=0", {
   # function from help for is.integer
   is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
 
-  d <- matrix(runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(dist=d, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
+  m <- NULL
+  d <- NULL
+  size <- c(1,10,100)
+  for (i in 1:3){
+    d[[i]] <- matrix(size[i]*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
+    m[[i]] <- map_maker_random(5,5,size[i])
 
-  d <- matrix(10*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(dist=d, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
+    for (bas in c(2,7,77)){
+      for (div in c(1:3,10,133)){
+        hi <- dist_to_hi_titre(dist=d[[i]], base=bas, divisor=div)
+        expect_equivalent(is.wholenumber(bas*hi$lessthanhack_titre/div), matrix(TRUE, ncol=5, nrow=5))
+        expect_equivalent(is.wholenumber(log(bas*hi$lessthanhack_titre/div,bas)), matrix(TRUE, ncol=5, nrow=5))
 
-  d <- matrix(100*runif(25),ncol=5, dimnames=list(paste0("AG", 1:5), paste0("SR", 1:5)))
-  hi <- dist_to_hi_titre(dist=d, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(dist=d, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
+        hi <- dist_to_hi_titre(m[[i]], base=bas, divisor=div)
+        expect_equivalent(is.wholenumber(bas*hi$lessthanhack_titre/div), matrix(TRUE, ncol=5, nrow=5))
+        expect_equivalent(is.wholenumber(log(bas*hi$lessthanhack_titre/div,bas)), matrix(TRUE, ncol=5, nrow=5))
+      }
+    }
 
-  m <- map_maker_random(5,5,100)
-  hi <- dist_to_hi_titre(m, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(m, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
 
-  m <- map_maker_random(5,5,10)
-  hi <- dist_to_hi_titre(m, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(m, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
 
-  m <- map_maker_random(5,5,1)
-  hi <- dist_to_hi_titre(m, base=2, divisor=10)
-  expect_equivalent(is.wholenumber(hi$hi_titre/10), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/10,2)), matrix(TRUE, ncol=5, nrow=5))
-  hi <- dist_to_hi_titre(m, base=77, divisor=3)
-  expect_equivalent(is.wholenumber(hi$hi_titre/3), matrix(TRUE, ncol=5, nrow=5))
-  expect_equivalent(is.wholenumber(log(hi$hi_titre/3,77)), matrix(TRUE, ncol=5, nrow=5))
+  }
+
+
+
+
 
 })
